af84bc9e2712f92da5ebddeebe8a5c37cfbffb77
Niccee14 <mankinennico@gmail.com>
2024-01-31 16:18:02 +0200

    Added function to pick randomly from 6 workouts

--- workout.js
+++ workout.js
@@ -1,1 +1,16 @@
-console.log("Hello World")
+
+// Variables containing different movements for different body-parts
+const upperBodyLow = ['Bench Press', 'Barbell Row', 'Chin Up' ];    //Low Volume, rep range of 10-5.
+const upperBodyHigh = ['Flies', 'Machine Row', 'SkullCrusher', 'Cable Up Row'];     //High Volume, rep range of 10-15.
+const ArmsAndShouldersLow = ['Overhead Press', 'Close Grip Bench', 'Ez Curl'];      //Low Volume, rep range of 12-8.
+const ArmsAndShouldersHigh = ['Rope pressdown', 'Cable Lateral Raise', 'Dumbell Lateral Raise', 'Hammer Rope Curl'];    //High Volume, rep range of 10-15.
+const legsLow = ['Squat', 'Romanian Deadlift', 'Hip Thrust', 'Deadlift', 'Bulgarian Split Squat'];      //Low volume, rep range of 10-5.
+const LegsHigh = ['Leg extensions', 'Leg Curl', 'Calf Raise'];      //High volume, rep range of 10-15.
+const arr = [upperBodyLow, upperBodyHigh, ArmsAndShouldersLow, ArmsAndShouldersHigh, legsLow, LegsHigh];
+const getRandomExercise = arr => {
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    const item = arr[randomIndex];
+    return item
+};
+
+console.log(getRandomExercise(arr))
